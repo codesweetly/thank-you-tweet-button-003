10ce2a267676bdf3797a0f1af266326131c36055
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-04-08 23:00:07 +0100

    feat: make users provide a rating
    BREAKING CHANGE: This commit breaks compatibility with the previous version

--- tweetButton.js
+++ tweetButton.js
@@ -1,4 +1,36 @@
-export function tweetButton(quantity) {
+function generateStarIcons(rating) {
+  const megaStars = `⭐⭐⭐⭐⭐ x ${Math.round(rating / 5)}`;
+  let stars = "";
+
+  for (let i = 0; i < rating; i++) {
+    stars += "⭐";
+  }
+  return rating > 10 ? megaStars : stars;
+}
+
+function showError() {
+  const errorContainer = document.createElement("div");
+  const errorParagraph = document.createElement("p");
+  const errorParagraphText = document.createTextNode(
+    "ⓘ Error: 'rating' parameter is missing in 'tweetButtonParameters'."
+  );
+
+  errorContainer.setAttribute(
+    "style",
+    "display: flex; justify-content: center; margin-top: 30px"
+  );
+  errorParagraph.setAttribute(
+    "style",
+    "background-color: #DC3545; color: #fff; width: 35%; border-radius: 5px; padding: 15px 20px"
+  );
+
+  errorParagraph.appendChild(errorParagraphText);
+  errorContainer.appendChild(errorParagraph);
+
+  return errorContainer;
+}
+
+export function tweetButton(quantity, rating) {
   const tweetButtonContainer = document.createElement("section");
   const anchorElement = document.createElement("a");
   const anchorElementContent = document.createTextNode(
@@ -8,7 +40,9 @@ export function tweetButton(quantity) {
     quantity && quantity > 1 ? quantity : "an"
   }+NPM+${
     quantity && quantity > 1 ? "packages" : "package"
-  }.%0A%0ACreating%20NPM%20Package:%20React%20JavaScript%20Guide%0A%0Ahttps%3A%2F%2Famzn.to/4lifL3n`;
+  }.%0A%0ABook's+Rating:+${rating}-star+rating!+${generateStarIcons(
+    rating
+  )}+%0A%0ACreating%20NPM%20Package:%20React%20JavaScript%20Guide%0A%0Ahttps%3A%2F%2Famzn.to/4lifL3n`;
 
   tweetButtonContainer.setAttribute("id", "tweet-btn-container");
   anchorElement.setAttribute("class", "tweet-button");
@@ -18,5 +52,5 @@ export function tweetButton(quantity) {
   anchorElement.appendChild(anchorElementContent);
   tweetButtonContainer.appendChild(anchorElement);
 
-  return tweetButtonContainer;
+  return typeof rating === "number" ? tweetButtonContainer : showError();
 }
